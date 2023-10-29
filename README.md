7603893642f689973398c8e00fde6900dac2fa69
Austin Nason <austin.nason@schrodinger.com>
2023-10-29 14:14:38 -0700

    .

--- README.md
+++ README.md
@@ -11,7 +11,16 @@ It's a bit awkward at the moment.
 
 ```shell
 sudo mv /etc/nix/nix.conf /etc/nix/nix.conf.orig
+mkdir -p ~/.config/nix-darwin
+cd ~/.config/nix-darwin
+git clone github.com/na-son/system-flakes .
 nix run nix-darwnin --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake .#luna
+```
+Open a new terminal, and run
+
+```shell
+cd ~/.config/nix/darwin
+darwin-rebuild switch --flake .#luna
 ```
 
 Nixos is simpler.
--- flake.nix
+++ flake.nix
@@ -1,127 +1,40 @@
 {
-  inputs = {
-    # Principle inputs (updated by `nix run .#update`)
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:lnl7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  description = "luna";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixos-flake.url = "github:na-son/unified";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, ... }:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      imports = [
-        inputs.nixos-flake.flakeModule
-      ];
 
-      flake =
-        let
-          myUserName = "nason";
-        in
-        {
-          # Configurations for Linux (NixOS) machines
-          nixosConfigurations = {
-            hecate = self.nixos-flake.lib.mkLinuxSystem {
-              nixpkgs.hostPlatform = "x86_64-linux";
-              imports = [
-                self.nixosModules.common # See below for "nixosModules"!
-                self.nixosModules.linux
-                # Your machine's configuration.nix goes here
-                ({ pkgs, ... }: {
-                  # TODO: Put your /etc/nixos/hardware-configuration.nix here
-                  boot.loader.grub.device = "nodev";
-                  fileSystems."/" = {
-                    device = "/dev/disk/by-label/nixos";
-                    fsType = "btrfs";
-                  };
-                  system.stateVersion = "23.05";
-                })
-                # Your home-manager configuration
-                self.nixosModules.home-manager
-                {
-                  home-manager.users.${myUserName} = {
-                    imports = [
-                      self.homeModules.common # See below for "homeModules"!
-                      self.homeModules.linux
-                    ];
-                    home.stateVersion = "22.11";
-                  };
-                }
-              ];
-            };
-          };
 
-          # Configurations for macOS machines
-          darwinConfigurations = {
-            luna = nix-darwin.lib.darwinSystem {
-              nixpkgs.hostPlatform = "aarch64-darwin";
-              modules = [ ./darwin.nix ];
-              imports = [
-                self.nixosModules.common # See below for "nixosModules"!
-                self.nixosModules.darwin
-                # Your machine's configuration.nix goes here
-                #({ pkgs, ... }: {
+outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # Auto upgrade nix package and the daemon service.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      nix.package = pkgs.nix;
+      nix.settings.experimental-features = "nix-command flakes";
+      services.nix-daemon.enable = true;
 
-                #  users.users.${myUserName} = {
-                #    home = "/Users/${myUserName}";
-                #  };
-                #})
-                # Your home-manager configuration
-                self.darwinModules.home-manager
-                {
-                  home-manager.users.${myUserName} = {
-                    imports = [
-                      self.homeModules.common # See below for "homeModules"!
-                      self.homeModules.darwin
-                    ];
-                    home.stateVersion = "22.11";
-                  };
-                }
-              ];
-            };
-          };
+      system.stateVersion = 4;
+      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-          # All nixos/nix-darwin configurations are kept here.
-          nixosModules = {
-            # Common nixos/nix-darwin configuration shared between Linux and macOS.
-            common = { pkgs, ... }: {
-              nixpkgs.config.allowUnfree = true;
-              environment.systemPackages = with pkgs; [
-                vim
-              ];
-            };
-            # NixOS specific configuration
-            linux = { pkgs, ... }: {
-              users.users.${myUserName}.isNormalUser = true;
-              services.netdata.enable = true;
-            };
-            # nix-darwin specific configuration
-            darwin = { pkgs, ... }: {
-              security.pam.enableSudoTouchIdAuth = true;
-            };
-          };
-
-          # All home-manager configurations are kept here.
-          homeModules = {
-            # Common home-manager configuration shared between Linux and macOS.
-            common = { pkgs, ... }: {
-              programs.git.enable = true;
-              programs.zsh.enable = true;
-            };
-            # home-manager config specific to NixOS
-            linux = {
-              xsession.enable = true;
-            };
-            # home-manager config specifi to Darwin
-            darwin = {
-              targets.darwin.search = "Google";
-            };
-          };
-        };
+      programs.zsh.enable = true;
     };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#luna
+    # Or switch using:
+    # $ darwin-rebuild switch --flake .#luna
+    darwinConfigurations."luna" = nix-darwin.lib.darwinSystem {
+      modules = [ ./configuration.nix ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."luna".pkgs;
+  };
 }
 
